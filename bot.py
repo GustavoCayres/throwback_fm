248d8174bbda8b918780589b451233958555a015
Gustavo Cayres <gustavo.rodrigues.cayres@gmail.com>
2019-09-19 23:23:47 -0300

    Fix webhook usage

--- bot.py
+++ bot.py
@@ -12,8 +12,19 @@ def main():
     for command in AVAILABLE_COMMANDS:
         dp.add_handler(CommandHandler(*command))
 
-    updater.start_webhook(webhook_url="https://throwback-fm.herokuapp.com/")
     print("Pooling for chat messages and idling...", file=sys.stderr, flush=True)
+
+    environment = os.environ["ENVIRONMENT"]
+    if environment == "DEVELOPMENT":
+        updater.start_polling()
+    elif environment == "HEROKU":
+        updater.start_webhook(listen="0.0.0.0",
+                              port=os.environ["PORT"],
+                              url_path=os.environ["BOT_TOKEN"])
+        updater.bot.set_webhook(F"{os.environ['HEROKU_APP_URL']}{os.environ['BOT_TOKEN']}")
+    else:
+        raise Exception(f"Unknown environment {environment}")
+
     updater.idle()
 
 
